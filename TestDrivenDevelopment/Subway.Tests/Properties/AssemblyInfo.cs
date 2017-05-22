169e290dc3f37bfecc93bcc85034ceb282b71b88
Ivan Prgomet <ivan_prgomet@hotmail.com>
2017-05-22 11:16:38 +0200

    structure

--- TestDrivenDevelopment/Subway.Tests/Properties/AssemblyInfo.cs
+++ TestDrivenDevelopment/Subway.Tests/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("6ef5973b-4eae-41f8-b1f7-a3cf76cc1b1f")]
+[assembly: Guid("13948ccf-fc2e-4e4b-acc9-2c67d3ab9191")]
 
 // Version information for an assembly consists of the following four values:
 //
