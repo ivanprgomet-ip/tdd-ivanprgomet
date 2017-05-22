1b5551b945af894f2d4c7f2a800b1ef7fc7937fc
Ivan Prgomet <ivan_prgomet@hotmail.com>
2017-05-22 11:42:17 +0200

    checking for email containing empty or null string

---
+++ TestDrivenDevelopment/ValidationEngineTests/Validator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ValidationEngineTests
+{
+    internal class Validator
+    {
+        internal bool ValidateEmailAddress(string email)
+        {
+            if (string.IsNullOrEmpty(email))
+                return false;
+            return true;
+        }
+    }
+}
