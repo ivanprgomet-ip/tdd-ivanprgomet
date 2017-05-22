4ff28c075490b1002d14a47ffb0ac960f3dbc769
Ivan Prgomet <ivan_prgomet@hotmail.com>
2017-05-22 15:03:22 +0200

    check for .com or .se in email string

--- TestDrivenDevelopment/ValidationEngineTests/Validator.cs
+++ TestDrivenDevelopment/ValidationEngineTests/Validator.cs
@@ -6,10 +6,22 @@ namespace ValidationEngineTests
     {
         internal bool ValidateEmailAddress(string email)
         {
+            string[] chunks = email.Split('@');
+
             if (string.IsNullOrEmpty(email))
                 return false;
             if (!email.Contains("@"))
                 return false;
+
+            // returns dotstring that should be .com or .se
+            int indexOfDot = chunks[1].IndexOf('.'); // index of the . in the second part of the email address (after the at symbol)
+            string dotString = chunks[1].Substring(indexOfDot); // eg. ".com" or ".se"
+            if (dotString != ".com")
+                return false;
+            if (dotString != ".se")
+                return false;
+
+
             return true;
         }
     }
