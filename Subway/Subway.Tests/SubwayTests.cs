3ba6bed1ec4aa64b5de44f44da41e61f4eec53ef
Ivan Prgomet <ivan_prgomet@hotmail.com>
2017-05-22 10:00:48 +0200

    added testmethods and exception class

---
+++ Subway/Subway.Tests/SubwayTests.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Subway.Tests
+{
+    [TestFixture]
+    public class SubwayTests
+    {
+        [Test]
+        public void TouchingInDecrementsBalance()
+        {
+            var sut = new TravelCard(15);
+            // Touch in/out a couple of times sut.TouchIn(); sut.TouchOut(); sut.TouchIn(); sut.TouchOut();
+            Assert.AreEqual(13, sut.TravelBalance, "Got decremented balance");
+        }
+
+
+        [Test]
+        public void MultipleTouchInsNotAllowed()
+        {
+            var sut = new TravelCard(5);
+            Assert.Throws<AlreadyTouchedInException>(() => { sut.TouchIn(); sut.TouchIn(); });
+        }
+
+
+    }
+}
---
+++ Subway/Subway.Tests/TravelCard.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Subway.Tests
+{
+    internal class TravelCard
+    {
+        private int v;
+        public decimal TravelBalance;
+
+        public TravelCard(int v)
+        {
+            this.v = v;
+        }
+
+        internal void TouchIn()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
