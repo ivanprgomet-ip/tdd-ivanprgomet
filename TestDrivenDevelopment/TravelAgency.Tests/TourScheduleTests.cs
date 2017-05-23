b2f561cff5530a2b30e6c30e40c3d9e40b1239ef
Ivan Prgomet <ivan_prgomet@hotmail.com>
2017-05-23 22:27:54 +0200

    setup

--- TestDrivenDevelopment/TravelAgency.Tests/TourScheduleTests.cs
+++ TestDrivenDevelopment/TravelAgency.Tests/TourScheduleTests.cs
@@ -10,16 +10,27 @@ namespace TravelAgency.Tests
     [TestFixture]
     public class TourScheduleTests
     {
+        private TourSchedule sut;
+        [SetUp]
+        public void Setup()
+        {
+            sut = new TourSchedule();
+        }
+
         [Test]
         public void CanCreateNewTour()
         {
-            TourSchedule sut = new TourSchedule();
-
             sut.CreateTour("New years day safari", new DateTime(2013, 1, 1), 20);
 
             List<Tour> tours = sut.GetToursFor(new DateTime(2013, 1, 1)); // returns all available tours on a given date
 
             Assert.AreEqual(1, tours.Count);
+        }
+
+        [Test]
+        public void ToursAreScheduledByDateOnly()
+        {
+            sut.CreateTour("New years day safari", new DateTime(2013, 1, 1, 10, 15, 0), 20);
 
         }
     }
