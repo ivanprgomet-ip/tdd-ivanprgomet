bbf93c50867064d4d5f41197ed8fd862c4331bf5
Ivan Prgomet <ivan_prgomet@hotmail.com>
2017-05-23 18:01:12 +0200

    refactor

--- TestDrivenDevelopment/CoinChangerApplication.Tests/CoinChanger.cs
+++ TestDrivenDevelopment/CoinChangerApplication.Tests/CoinChanger.cs
@@ -15,22 +15,19 @@ namespace CoinChangerApplication.Tests
 
         internal Dictionary<decimal, int> MakeChange(decimal turnMeIntoChangeAmount)
         {
-            // sort first
             coinTypes = coinTypes.OrderByDescending(c => c).ToList();
 
-            // initialize result dictionary 
             Dictionary<decimal, int> myChange = InitializeWithCointypes(coinTypes);
 
             foreach (var coinType in coinTypes)
             {
                 if (turnMeIntoChangeAmount % coinType != 0)
                 {
-                    // eg. turnmeintochangeamount = 14, cointype = 5
-                    decimal rest = turnMeIntoChangeAmount % coinType;  // rest = 4
-                    decimal evenSum = turnMeIntoChangeAmount - rest; // evenSum = 10
-                    decimal coinTypeCount = evenSum / coinType; // coinTypeCount = 2
-                    myChange[coinType] = (int)coinTypeCount; // 2 5's
-                    turnMeIntoChangeAmount -= evenSum; // 14-10 = 4 (update how much is left of the turnMeIntoChangeAmount)
+                    decimal rest = turnMeIntoChangeAmount % coinType;
+                    decimal evenSum = turnMeIntoChangeAmount - rest;
+                    decimal coinTypeCount = evenSum / coinType;
+                    myChange[coinType] = (int)coinTypeCount;
+                    turnMeIntoChangeAmount -= evenSum;
                 }
                 else
                 {
