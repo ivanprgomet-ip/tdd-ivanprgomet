4749f9e5e2e82ad6128be8902fcddfa27e62eef0
Ivan Prgomet <ivan_prgomet@hotmail.com>
2017-05-23 09:16:26 +0200

    gui for testing emails done

--- TestDrivenDevelopment/ValidationEngine/Program.cs
+++ TestDrivenDevelopment/ValidationEngine/Program.cs
@@ -10,6 +10,26 @@ namespace ValidationEngine
     {
         static void Main(string[] args)
         {
+            /// Sample error cases:
+            ///     [x] Null or empty string
+            ///     [x] Test.com
+            ///     [x] name@test
+            ///     [x] name.test@com
+            ///     [x] Name2015@test.com
+            ///     [x] name@test2015.com
+            /// Valid email examples:
+            ///     [x] mike@edument.se
+            ///     [x] joe@apple.com
+
+
+             Validator v = new Validator();
+
+            Console.WriteLine("Enter Email Address to validate it!");
+            string email = Console.ReadLine();
+            if (v.ValidateEmailAddress(email))
+                Console.WriteLine("Success");
+            else
+                Console.WriteLine("Validation failed");
         }
     }
 }
