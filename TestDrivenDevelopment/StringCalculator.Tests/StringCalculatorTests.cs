1d22125759200413d77344a14fb5005c34099cc9
Ivan Prgomet <ivan_prgomet@hotmail.com>
2017-05-24 17:22:52 +0200

    string calculator failing tests atm

---
+++ TestDrivenDevelopment/StringCalculator.Tests/StringCalculatorTests.cs
@@ -0,0 +1,49 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculator.Tests
+{
+    [TestFixture]
+    public class StringCalculatorTests
+    {
+        private StringCalculator sut;
+        [SetUp]
+        public void Setup()
+        {
+            // arrange
+            sut = new StringCalculator();
+        }
+
+
+
+        [Test]
+        public void ReturnZeroForAnEmtpyString()
+        {
+            // act
+            int sum = sut.Add("");
+
+            // assert
+            Assert.AreEqual(0, sum);
+        }
+
+        [Test]
+        public void AddingOneNumber()
+        {
+            int sum = sut.Add("5");
+
+            Assert.AreEqual(5, sum);
+        }
+
+        [Test]
+        public void AddingTwoNumbers()
+        {
+            int sum = sut.Add("5,5");
+
+            Assert.AreEqual(10, sum);
+        }
+    }
+}
