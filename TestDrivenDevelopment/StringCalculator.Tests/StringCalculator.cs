873692b5dad9b535d51cfa2cba03d22afe186dc7
Ivan Prgomet <ivan_prgomet@hotmail.com>
2017-05-25 09:57:12 +0200

    AddMethodWithOptionalCustomDelimiterCanBeOfAnyLength passing

--- TestDrivenDevelopment/StringCalculator.Tests/StringCalculator.cs
+++ TestDrivenDevelopment/StringCalculator.Tests/StringCalculator.cs
@@ -15,36 +15,68 @@ namespace StringCalculator.Tests
             else
             {
                 if (numbers.Contains("//"))
-                    return 3;
-
-                char[] tokens = new char[] { ',', '\n' };
-                string[] nums = numbers.Split(tokens);
-
-                int sum = 0;
-                bool negativeFound = false;
-                StringBuilder negatives = new StringBuilder();
-                foreach (var n in nums)
+                    return CalculateSumWithCustomDelimiter(numbers);
+                else
                 {
-                    if (int.Parse(n) < 0)
+
+                    char[] tokens = new char[] { ',', '\n' };
+                    string[] nums = numbers.Split(tokens);
+
+                    int sum = 0;
+                    bool negativeFound = false;
+                    StringBuilder negatives = new StringBuilder();
+                    foreach (var n in nums)
                     {
-                        negativeFound = true;
-                        negatives.Append(n + " ");
+                        if (int.Parse(n) < 0)
+                        {
+                            negativeFound = true;
+                            negatives.Append(n + " ");
+                        }
+                        else if (int.Parse(n) > 1000) // if current number is bigger than 1000, ignore it
+                            continue;
+                        else
+                            sum += int.Parse(n);
                     }
-                    else if (int.Parse(n) > 1000) // if current number is bigger than 1000, ignore it
-                        continue;
-                    else
-                        sum += int.Parse(n);
+
+
+                    if (negativeFound)
+                        throw new NegativesNotAllowedException("negatives not allowed " + negatives);
+                    //else if (1000 < sum)
+                    //    sum -= 1001;
+
+
+                    return sum;
                 }
-
-
-                if (negativeFound)
-                    throw new NegativesNotAllowedException("negatives not allowed " + negatives);
-                //else if (1000 < sum)
-                //    sum -= 1001;
-
-
-                return sum;
             }
+        }
+
+        private int CalculateSumWithCustomDelimiter(string numbers)
+        {
+            // get the custom delimiter from the string
+            int indexTwo = numbers.IndexOf("\n");// index of the first \n
+            int customDelimiterLength = indexTwo - 2;
+            string customDelimiter = numbers.Substring(2, customDelimiterLength); // 2 is the index where the custom delimiter starts
+
+            string[] nums = numbers.Split(new string[] { customDelimiter }, StringSplitOptions.None); // splitting by a string, and not a char!
+
+            int sum = ExtractSum(nums);
+
+            return sum;
+        }
+
+        private int ExtractSum(string[] nums)
+        {
+            int sum = 0;
+            foreach (var num in nums)
+            {
+                int n;
+                bool isNumeric = int.TryParse(num, out n);
+
+                if (isNumeric)
+                    sum += n;
+
+            }
+            return sum;
         }
     }
 }
